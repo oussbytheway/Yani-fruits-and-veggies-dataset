73b98aff731866a5619bea8d3dfd147b1fa1a050
oussamabenberkane <oussama.benberkane@tech-instinct.com>
2024-04-11 12:16:23 +0100

    fix commit

--- analyse.py
+++ analyse.py
@@ -1,7 +1,6 @@
 import os
 
 base_dir = "train"
-compare_dir = "test"
 
 # Function to count the number of files in a directory
 def count_files(directory):
@@ -9,19 +8,8 @@ def count_files(directory):
 
 for subdir in os.listdir(base_dir):
     base_subdir_path = os.path.join(base_dir, subdir)
-    compare_subdir_path = os.path.join(compare_dir, subdir)
-   
-    # Count the number of files in the train and test directories
+
     train_files = count_files(base_subdir_path)
-    test_files = count_files(compare_subdir_path)
-
-    # Calculate the total number of files
-    total_files = train_files + test_files
-
-    # Calculate the percentages
-    train_percentage = (train_files / total_files) * 100
-    test_percentage = (test_files / total_files) * 100
 
     # Print the results
-    print(f"Percentage of train files for {subdir}: {train_percentage:.2f}%")
-    print(f"Percentage of test files for {subdir}: {test_percentage:.2f}%")
+    print(f"Number of train files for {subdir}: {train_files}")
